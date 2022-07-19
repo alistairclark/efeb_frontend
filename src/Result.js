a40eae650d7383c14eb2542b9eaf1a9b97f7b075
Alistair Clark <alistairclark89@gmail.com>
2022-07-19 21:04:41 +0100

    Start styling buttons

--- src/Result.js
+++ src/Result.js
@@ -12,29 +12,23 @@ export default function Result (props) {
 
     return (
         <div className="component-result-row">
-            <img height="200" src={props.picture} alt={`${props.display_name}`}/>
-            <h2>{props.display_name}</h2>
-            <div>£{props.price}</div>
-            <div>{props.stock_count} in stock</div>
-            <div>{props.description}</div>
-            <div>{props.links}</div>
-            <div>{props.manufacturer}</div>
-            {props.categories.map(category => (
-                <div key={category.slug}>{category.display_name}</div>
-            ))}
-            {props.canAdd &&
-                <button onClick={handleAddToCart} value={JSON.stringify(props.object)}>Add to cart</button>
-            }
-            {props.canRemove &&
-                <button onClick={handleRemoveFromCart} value={JSON.stringify(props.object)}>Remove from cart</button>
-            }
-
             <Link
                 style={{ display: "block", margin: "1rem 0" }}
                 to={`/products/${props.slug}/`}
             >
-                See more
+                <img height="200" src={props.picture} alt={`${props.display_name}`}/>
             </Link>
+            <h2>{props.display_name}</h2>
+            <div>£{props.price} ({props.stock_count} in stock)</div>
+
+            <div className="addRemoveButtons">
+                {props.canAdd &&
+                    <button className="addRemove" onClick={handleAddToCart} value={JSON.stringify(props.object)}>Add</button>
+                }
+                {props.canRemove &&
+                    <button className="addRemove" onClick={handleRemoveFromCart} value={JSON.stringify(props.object)}>Remove</button>
+                }
+            </div>
         </div >
     );
 }
