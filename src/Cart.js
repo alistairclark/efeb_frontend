5ad0789ad5532a79e9a39b9208f958863fc719af
Alistair Clark <alistairclark89@gmail.com>
2022-07-25 12:32:36 +0100

    Improves cart styling

--- src/Cart.js
+++ src/Cart.js
@@ -35,26 +35,40 @@ export default function Cart (props) {
     }
 
     return (
+        <div>
+        {Object.keys(props.items).length !== 0 && 
         <div className="cart">
             <h2>Cart</h2>
             {Object.values(props.items).map((value) => {
                 return (
-                    <div key={value.data.slug}>{value.data.display_name} £{value.data.price * value.quantity}
-                        {canAdd(value.data) &&
-                            <button onClick={handleAddToCart} value={JSON.stringify(value.data)}>Add to cart</button>
-                        }
-                        {canRemove(value.data) &&
-                            <button onClick={handleRemoveFromCart} value={JSON.stringify(value.data)}>Remove from cart</button>
-                        }
+                    <div className="cart-item" key={value.data.slug}>
+                        <div className="cart-item-details">{value.data.display_name} (x {value.quantity})</div>
+                        <div className="cart-item-pricing">
+                            <span className="price">
+                                £{value.data.price * value.quantity}
+                            </span>
+                            <span className="add-button">
+                                {canAdd(value.data) &&
+                                    <button className="addRemove" onClick={handleAddToCart} value={JSON.stringify(value.data)}>+</button>
+                                }
+                            </span>
+                            <span className="remove-button">
+                                {canRemove(value.data) &&
+                                    <button className="addRemove" onClick={handleRemoveFromCart} value={JSON.stringify(value.data)}>-</button>
+                                }
+                            </span>
+                        </div>
                     </div>
                 )
             })}
-            Total: £{calculateTotal()}
+            <div className="cart-total">Total: £{calculateTotal()}</div>
 
             <form method="post" action={`${process.env.REACT_APP_BACKEND_URL}/checkout/`}>
                 <input type="hidden" name="cart" value={JSON.stringify(props.items)} />
-                <input type="submit" value="Checkout" />
+                <input className="checkout" type="submit" value="Checkout" />
             </form>
         </div >
+        }
+        </div>
     );
 }
